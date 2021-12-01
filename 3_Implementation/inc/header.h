495b19990a888f0abaa66cd2b381b4c0f2d52306
Yanadi Prudhvi <94187482+YanadiPrudhvi@users.noreply.github.com>
2021-12-01 22:09:59 +0530

    added header

---
+++ 3_Implementation/inc/header.h
@@ -0,0 +1,9 @@
+/**
+ * @brief Header file that include all functions used in this project.
+ * 
+ */
+void LCD_Init (void);
+void LCD_Action(unsigned char);
+void LCD_Clear(void);
+void LCD_Print (char*);
+void LCD_Printpos (char , char , char* );
